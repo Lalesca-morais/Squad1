9158d6289986fd33f62ae484d6ea58ed4fe8b29a
paula.nascimento <paula.nascimento@zup.com.br>
2023-10-19 11:31:21 -0300

    enviando para merge

--- src/main/java/br/com/zup/squad1/exeption/IngredientNotFound.java
+++
@@ -1,7 +0,0 @@
-package br.com.zup.squad1.exeption;
-
-public class IngredientNotFound extends Exception{
-    public IngredientNotFound(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/zup/squad1/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.zup.squad1.repository;
-import br.com.zup.squad1.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-
-}
