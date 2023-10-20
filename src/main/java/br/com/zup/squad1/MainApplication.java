f8636b154988abb8a7771e385195136d8d66cf6e
paula.nascimento <paula.nascimento@zup.com.br>
2023-10-20 08:46:57 -0300

    problema corrigido

--- src/main/java/br/com/zup/squad1/MainApplication.java
+++ src/main/java/br/com/zup/squad1/MainApplication.java
@@ -2,17 +2,8 @@ package br.com.zup.squad1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-//@SpringBootApplication
-//@ComponentScan("br.com.zup.squad1")
-@SpringBootApplication()
-@ComponentScan(basePackages = "br.com.zup.squad1.*")
-@EnableJpaRepositories(basePackages = "br.com.zup.squad1.*")
-@EntityScan(basePackages = "br.com.zup.squad1.*")
+@SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
