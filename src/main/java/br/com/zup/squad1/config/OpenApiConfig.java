21e35ffcb150a270bd7f59ea669db182ced1a443
vitoria.ferreira <vitoria.ferreira@zup.com.br>
2023-11-07 14:46:54 -0300

    updating swagger documentation information

--- src/main/java/br/com/zup/squad1/config/OpenApiConfig.java
+++ src/main/java/br/com/zup/squad1/config/OpenApiConfig.java
@@ -2,7 +2,6 @@ package br.com.zup.squad1.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,19 +9,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
     @Bean
-    public OpenAPI customOpenApi(){
+    public OpenAPI customOpenApi() {
         return new OpenAPI()
                 .components(new Components())
                 .info(new Info().title("API Squad 1")
-                        .description("API documentation developed by squad 1").version("1.0")
-                        .contact(new Contact().name("Developer Team: \n Amanda Santos" +
-                                        " \n Lalesca Silva " +
-                                        "\n Paula Nascimento " +
-                                        "\n Vitória Ferreira")
-                                .url("\n https://github.com/Lalesca-morais/Squad1.git \n")
-                                .email("amanda.santos@zup.com.br \n" +
-                                        "lalesca.silva@zup.com.br \n" +
-                                        "paula.nascimento@zup.com.br \n" +
-                                        "vitoria.ferreira@zup.com.br")));
+                        .description("Com o Sabor&Saúde , o usuário terá um controle mais efetivo dos ingredientes, reduzindo o desperdício de alimentos, e uma ferramenta que o auxilia a manter a saúde, acompanhando e garantindo a ingestão ideal de água diariamente. Isso não só traz mais eficiência na cozinha, mas também promove um estilo de vida mais saudável e sustentável.\n" +
+                                "\nEsperamos que a API Sabor&Saúde torne sua vida mais saudável, eficiente e sustentável.\n" +
+                                "\n Para obter detalhes sobre como utilizar nossa API, consulte a documentação completa em [GitHub](https://github.com/Lalesca-morais/Squad1.git)\n" +
+                                "\nDeveloper Team:\n\n" +
+                                " **Amanda Santos** (amanda.santos@zup.com.br)\n\n" +
+                                " **Lalesca Silva** (lalesca.silva@zup.com.br)\n\n" +
+                                " **Paula Nascimento** (paula.nascimento@zup.com.br)\n\n" +
+                                " **Vitória Ferreira** (vitoria.ferreira@zup.com.br)").version("1.0"));
     }
 }
