86d200da676ed8dd30a8b15b46cd6548b356ea06
Amanda.Santos <amanda.santos@zup.com.br>
2023-11-08 15:42:35 -0300

    editing API title in swagger

--- src/main/java/br/com/zup/squad1/config/OpenApiConfig.java
+++ src/main/java/br/com/zup/squad1/config/OpenApiConfig.java
@@ -12,7 +12,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenApi() {
         return new OpenAPI()
                 .components(new Components())
-                .info(new Info().title("API Squad 1")
+                .info(new Info().title("Sabor&Saúde API")
                         .description("Com o Sabor&Saúde , o usuário terá um controle mais efetivo dos ingredientes, reduzindo o desperdício de alimentos, além disso auxilia a manter a saúde, acompanhando e garantindo a ingestão ideal de água diariamente. Isso não só traz mais eficiência na cozinha, mas também promove um estilo de vida mais saudável e sustentável.\n" +
                                 "\nEsperamos que a API Sabor&Saúde torne sua vida mais saudável, eficiente e sustentável.\n" +
                                 "\n Para obter detalhes sobre como utilizar nossa API, consulte a documentação completa em [GitHub](https://github.com/Lalesca-morais/Squad1.git)\n" +
